c65c5ed140a526f669fbd85033dbec4c5efac101
Leonid Kuznetcov <leonidkuz18@gmail.com>
2020-10-11 19:33:25 +0300

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Леонид Кузнецов on 11.10.2020.
+//  Copyright © 2020 Leonid Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
