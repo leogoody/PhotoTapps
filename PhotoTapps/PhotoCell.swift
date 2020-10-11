fff860462aac7926ec5f5540e3fce8d6b8273463
Leonid Kuznetcov <leonidkuz18@gmail.com>
2020-10-11 23:17:31 +0300

    Add finished project, README.md and .gitignore

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Леонид Кузнецов on 11.10.2020.
+//  Copyright © 2020 Leonid Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var dogImageView: UIImageView!
+}
